b583d93520ded8325c37fc6cce7ca2f7a059a589
Mustafa Ceylan <mustafaceylanmain@gmail.com>
2024-05-25 17:51:49 +0300

    ilan detay sayfası yapıldı

---
+++ JobFinderApp/Controllers/UserControllers/UserAdvertisesViewController.swift
@@ -0,0 +1,43 @@
+//
+//  UserAdvertisesViewController.swift
+//  JobFinderApp
+//
+//  Created by Mustafa Ceylan on 25.05.2024.
+//
+
+import UIKit
+
+class UserAdvertisesViewController: UIViewController {
+
+    @IBOutlet weak var companyNameInput: UITextField!
+    @IBOutlet weak var advertiseTitleInput: UITextField!
+    @IBOutlet weak var jobNameInput: UITextField!
+    @IBOutlet weak var advertiseDescriptionInput: UITextView!
+    
+    var advertiseDetail : AdvertiseDetail?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+
+        companyNameInput.text = advertiseDetail?.company?.name
+        advertiseTitleInput.text = advertiseDetail?.advertise?.title
+        jobNameInput.text = advertiseDetail?.advertise?.jobId
+        advertiseDescriptionInput.text = advertiseDetail?.advertise?.description
+        
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+
+    }
+    
+    @IBAction func sendAdvertiseApplicationButton(_ sender: Any) {
+        
+    }
+}
--- JobFinderApp/Controllers/UserControllers/UserHomeViewController.swift
+++ JobFinderApp/Controllers/UserControllers/UserHomeViewController.swift
@@ -1,22 +1,17 @@
-//
-//  UserHomeViewController.swift
-//  JobFinderApp
-//
-//  Created by Mustafa Ceylan on 28.04.2024.
-//
-
 import UIKit
 import GoogleMaps
 import CoreLocation
 
 class UserHomeViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDelegate {
-
+    
     @IBOutlet weak var homeMapView: UIView!
     var advertiseDetails: [AdvertiseDetail] = []
-
+    
     let locationManager = CLLocationManager()
     let googleMapView = GMSMapView()
-
+    var selectedMarker: GMSMarker?
+    var customInfoWindow: UIView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         googleMapView.delegate = self
@@ -26,7 +21,7 @@ class UserHomeViewController: UIViewController, CLLocationManagerDelegate, GMSMa
             await fetchCompaniesFromDatabase()
         }
     }
-
+    
     func setupMap() {
         let camera = GMSCameraPosition.camera(withLatitude: 41.015137, longitude: 28.979530, zoom: 8.0)
         googleMapView.camera = camera
@@ -34,7 +29,7 @@ class UserHomeViewController: UIViewController, CLLocationManagerDelegate, GMSMa
         googleMapView.isMyLocationEnabled = true
         self.homeMapView.addSubview(googleMapView)
     }
-
+    
     func getCurrentLocation() {
         self.locationManager.requestAlwaysAuthorization()
         self.locationManager.requestWhenInUseAuthorization()
@@ -45,45 +40,106 @@ class UserHomeViewController: UIViewController, CLLocationManagerDelegate, GMSMa
             locationManager.startUpdatingLocation()
         }
     }
-
-    // CLLocationManagerDelegate method
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last {
             let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude, longitude: location.coordinate.longitude, zoom: 10.0)
             googleMapView.animate(to: camera)
         }
     }
- 
     
-
     func addCompanyMarker(advertiseDetail: AdvertiseDetail) {
-        
         guard let latitude = advertiseDetail.company?.locationLat, let longitude = advertiseDetail.company?.locationLong else { return }
         
         let position = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
         let marker = GMSMarker(position: position)
         marker.title = advertiseDetail.company?.name
-        marker.snippet = advertiseDetail.company?.address
-        marker.userData = advertiseDetail // AdvertiseDetail nesnesini marker.userData'ya ekle
+        marker.snippet = advertiseDetail.advertise?.title
+        marker.userData = advertiseDetail
         marker.map = googleMapView
     }
-
+    
     func fetchCompaniesFromDatabase() async {
         do {
-            // Veritabanından reklam detaylarını alın
             advertiseDetails = try await AdvertiseService().GetAllAdvertiseDetail()
             await MainActor.run {
-                googleMapView.clear() // Mevcut tüm markerları kaldır
+                googleMapView.clear()
                 displayCompaniesOnMap()
             }
         } catch {
             print("Error fetching companies: \(error)")
         }
     }
-
+    
     func displayCompaniesOnMap() {
         for advertiseDetail in advertiseDetails {
             addCompanyMarker(advertiseDetail: advertiseDetail)
+        }
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+        selectedMarker = marker
+        
+        if let customInfoWindow = customInfoWindow {
+            customInfoWindow.removeFromSuperview()
+        }
+        
+        customInfoWindow = createCustomInfoWindow(for: marker)
+        if let customInfoWindow = customInfoWindow {
+            self.view.addSubview(customInfoWindow)
+            customInfoWindow.center = self.view.center
+        }
+        
+        return true // Return true to indicate we've handled the tap
+    }
+    
+    func createCustomInfoWindow(for marker: GMSMarker) -> UIView {
+        let infoWindow = UIView()
+        infoWindow.frame = CGRect(x: 0, y: 0, width: 200, height: 150)
+        infoWindow.backgroundColor = UIColor.white
+        infoWindow.layer.cornerRadius = 8
+        infoWindow.layer.masksToBounds = true
+        
+        // Title label
+        let titleLabel = UILabel()
+        titleLabel.frame = CGRect(x: 8, y: 8, width: infoWindow.frame.width - 16, height: 20)
+        titleLabel.text = marker.title
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        infoWindow.addSubview(titleLabel)
+        
+        // Snippet label
+        let snippetLabel = UILabel()
+        snippetLabel.frame = CGRect(x: 8, y: 36, width: infoWindow.frame.width - 16, height: 40)
+        snippetLabel.text = marker.snippet
+        snippetLabel.font = UIFont.systemFont(ofSize: 14)
+        snippetLabel.numberOfLines = 0
+        infoWindow.addSubview(snippetLabel)
+        
+        // Action button
+        let actionButton = UIButton(type: .system)
+        actionButton.frame = CGRect(x: 8, y: 76, width: infoWindow.frame.width - 16, height: 50)
+        actionButton.setTitle("İlan Detayları", for: .normal)
+        actionButton.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        actionButton.addTarget(self, action: #selector(infoWindowButtonTapped(_:)), for: .touchUpInside)
+        infoWindow.addSubview(actionButton)
+        
+        return infoWindow
+    }
+    
+    @objc func infoWindowButtonTapped(_ sender: UIButton) {
+        if let selectedMarker = selectedMarker, let advertiseDetail = selectedMarker.userData as? AdvertiseDetail {
+            if let userAdvertisesVC = storyboard?.instantiateViewController(withIdentifier: "UserAdvertisesViewController") as? UserAdvertisesViewController {
+                userAdvertisesVC.advertiseDetail = advertiseDetail
+                navigationController?.setNavigationBarHidden(true, animated: false)
+                navigationController?.pushViewController(userAdvertisesVC, animated: true)
+            }
+        }
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        if let customInfoWindow = customInfoWindow {
+            customInfoWindow.removeFromSuperview()
+            self.customInfoWindow = nil
         }
     }
 }
